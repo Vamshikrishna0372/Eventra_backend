116496bcd6e26084831480f26ccec74262748f69
Vamshikrishna0372 <nagulavamshi1453@gmail.com>
2026-03-15 00:26:23 +0530

    Fix CORS allowing all origins for production

--- app/main.py
+++ app/main.py
@@ -31,16 +31,7 @@ logger = logging.getLogger("eventra")
 # Setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://[::1]:8080",
-        "http://[::1]:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
